efafbe490fc4902e3edbc3b2b4f9a0ff50b15464
pablo.mayrgundter <pablo.mayrgundter@b8c78ab8-d18f-11de-aacf-132551bdafba>
2009-12-01 05:43:36 +0000

    Refactored phys and fixed grav rings.

---
+++ trunk/phys/ColoredBlob.java
@@ -0,0 +1,29 @@
+package phys;
+
+import java.awt.Color;
+
+final class ColoredBlob extends Blob {
+
+  static final String COLORS_CSV = System.getProperty("colors", "black");
+  static final Color [] COLORS;
+  static {
+    String [] colors = COLORS_CSV.split(",");
+    COLORS = new Color[colors.length];
+    for (int i = 0; i < colors.length; i++) {
+      try {
+      java.lang.reflect.Field f = Color.class.getDeclaredField(colors[i].toUpperCase());
+      COLORS[i] = (Color)f.get(Color.class);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  Color color;
+  void setMass(final float mass) {
+    this.mass = mass;
+    final int ndx = (int)((float)mass / (MAXMASS * 1.1f) * (float)COLORS.length);
+    color = COLORS[ndx];
+    radius = (int) (mass * SIZE);
+  }
+}
