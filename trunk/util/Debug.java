1883d32716265df1ff0bb2a8e9e68cebc4ca825f
pablo.mayrgundter <pablo.mayrgundter@b8c78ab8-d18f-11de-aacf-132551bdafba>
2009-12-17 19:58:31 +0000

    Added SolarSystem3d app.

---
+++ trunk/util/Debug.java
@@ -0,0 +1,11 @@
+package util;
+
+public class Debug {
+  public static boolean ON = false;
+  public static boolean p(final String s) {
+    return true;
+  }
+  public static boolean trace(final Exception e) {
+    return true;
+  }
+}
