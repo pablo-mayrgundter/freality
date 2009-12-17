ab6ea59a74d6e8da90bfd602a78cdbcd209462a4
pablo.mayrgundter <pablo.mayrgundter@b8c78ab8-d18f-11de-aacf-132551bdafba>
2009-12-17 18:19:00 +0000

    Added grav3d.jar creation script.

---
+++ Grav3dJar.sh
@@ -0,0 +1,1 @@
+jar cmf META-INF/MANIFEST.MF grav3d.jar phys/{Grav*,Phys*,GravSim*,Point*,Blob*,ColoredBlob*,Display*}.{java,class} gfx/Display3D*.{java,class} gfx/FullScreenableFrame.{java,class} org/freality/gui vr/cpack/space/SpaceShipNavigator.*
