df1ab96533a15e2b2a7aaf2d597df69b29ce3f36
pablo.mayrgundter <pablo.mayrgundter@b8c78ab8-d18f-11de-aacf-132551bdafba>
2010-06-17 19:39:29 +0000

    Make these work for Mac bash again.

--- fun/mondrian.sh
+++ fun/mondrian.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 export TERM=linux
-COLUMNS=$1
-LINES=$2
+COLUMNS=${COLUMNS:-$1}
+LINES=${LINES:-$2}
 while true ; do
-    x=$((RANDOM%COLUMNS))
-    y=$((RANDOM%LINES))
-    c=$((40+(RANDOM%10)))
-    width=$((1+(RANDOM%((COLUMNS-x)+1))))
-    height=$((1+(RANDOM%((LINES-y)+1))))
-    h=$y;
-    while [ $h -lt $height ] ; do
-        w=$x;
-        while [ $w -lt $width ] ; do
-            xx=$[ x+w ]
-            echo -en "\033[$yy;${xx}f\033[1;${c}m "
-            w=$[ w + 1 ]
-        done
-        yy=$[ y+h ]
-        h=$[ h + 1 ]
+  x=$[RANDOM % COLUMNS]
+  y=$[RANDOM % LINES]
+  c=$[40+RANDOM%10]
+  width=$[1 + RANDOM % COLUMNS - x + 1]
+  height=$[1 + RANDOM % LINES - y + 1]
+  h=$y;
+  while [ $h -lt $height ] ; do
+    w=$x;
+    while [ $w -lt $width ] ; do
+      xx=$[ x+w ]
+      echo -en "\033[$yy;${xx}f\033[1;${c}m "
+      w=$[ w + 1 ]
     done
+    yy=$[ y+h ]
+    h=$[ h + 1 ]
+  done
 done
