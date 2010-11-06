2aad0317525d0d7c2fb1e29af9bb6fe91d92294c
pablo.mayrgundter <pablo.mayrgundter@b8c78ab8-d18f-11de-aacf-132551bdafba>
2010-11-06 20:53:01 +0000

    Lots of refactoring.

--- fun/game/GOL.js
+++ fun/game/GOL.js
@@ -1,26 +1,31 @@
 /**
  * A hardcoded implementation of Conway's Game of Life for testing.
- * Any.js (will) contains the generic rule system for which GOL is a
+ * Any.js will contain the generic rule system for which GOL is a
  * special case.
  */
-function runRules() {
-  for (var y = 1; y < rows - 1; y++) {
-    for (var x = 1; x < cols - 1; x++) {
+function GOL(board) {
+  this.board = board;
+}
+
+GOL.prototype.runRules = function() {
+  var b = this.board;
+  for (var y = 1; y < b.getSize() - 1; y++) {
+    for (var x = 1; x < b.getSize() - 1; x++) {
       var count = 0;
       for (var i = -1; i < 2; i++)
         for (var j = -1; j < 2; j++)
-          if (isOn(x + j, y + i)) {
+          if (b.isOn(x + j, y + i)) {
             if (i == 0 && j == 0)
               continue;
             count++;
           }
       switch (count) {
-      case 0: ;
-      case 1: turnOff(x, y); break;
+      case 0: if (b.isOn(x,y)) { b.turnOff(x, y) }; break;
+      case 1: if (b.isOn(x,y)) { b.turnOff(x, y) }; break;
       case 2: break;
-      case 3: if (!isOn(x, y)) turnOn(x, y); break;
+      case 3: if (!b.isOn(x, y)) b.turnOn(x, y); break;
       case 4:;
-      case 5: turnOff(x, y);
+      case 5: b.turnOff(x, y);
       }
     }
   }
--- fun/game/Game.js
+++ fun/game/Game.js
@@ -1,84 +1,12 @@
-var canvasElt;
-var canvasWidth, canvasHeight;
-var c;
-var size = 20, cW, cH;
-var rows;
-var cols;
-var board = new Array();
-var white = 'rgb(255,255,255)';
-var black = 'rgb(0,0,0)';
+var canvas, game;
 
 function init() {
-
-  canvasElt = document.getElementById('game');
-  canvas = canvasElt.getContext('2d');
-  cW = canvasElt.width;
-  cH = canvasElt.height;
-  rows = Math.floor(cH / size);
-  cols = Math.floor(cW / size);
-  //alert(rows+','+cols);
-
-  for (var y = 0; y < rows; y++) {
-    board[y] = new Array();
-    for (var x = 0; x < cols; x++) {
-      board[y][x] = {'set': 0};
-    }
-  }
-
-  for (var y = 0; y < rows; y++) {
-    for (var x = 0; x < cols; x++) {
-      set(x, y, 'color', white);
-    }
-  }
-
-  addEvent(canvasElt, 'click', clickHandler);
+  var boardSize = 25;
+  var board = new Board(boardSize);
+  canvas = new Canvas(board);
+  game = new GOL(board);
+  addEvent(canvas.canvasElt, 'click', func(canvas, canvas.clickHandler));
   addEvent(get('button'), 'click', buttonHandler);
-};
-
-function clickHandler(evt) {
-  var x = evt.x - canvasElt.offsetLeft;
-  var y = evt.y - canvasElt.offsetTop;
-  var dx = Math.floor(x / cW * cols);
-  var dy = Math.floor(y / cH * rows);
-  if (isSet(dx, dy, 'color') == black)
-    set(dx, dy, 'color', white);
-  else
-    set(dx, dy, 'color', black);
-  draw();
-}
-
-function isOn(x, y) {
-  return isSet(x, y, 'color') == black;
-}
-function turnOn(x, y) {
-  set(x, y, 'newColor', black);
-}
-function turnOff(x, y) {
-  set(x, y, 'newColor', white);
-}
-
-function set(x, y, name, value) {
-  if (board[x][y]['newColor'])
-    board[x][y]['newColor'] = undefined;
-  board[x][y][name] = value;
-};
-
-function checkBoard(msg, x, y, name) {
-  if (!board)
-    alert(msg+' !board');
-  else if (!board[x])
-    alert(msg+' !board['+x+']');
-  else if (!board[x][y])
-    alert(msg+' !board['+x+']['+y+']');
-  else if (!board[x][y][name])
-    alert(msg+' !board['+x+']['+y+']['+name+']');
-}
-
-function isSet(x, y, name) {
-  //  checkBoard('isSet', x, y, name);
-  if (!board[x][y][name])
-    return false;
-  return board[x][y][name];
 };
 
 var running = false;
@@ -86,25 +14,12 @@ var running = false;
 function buttonHandler() {
   running = !running;
   animate();
-}
-
-function animate() {
-  runRules();
-  draw();
-  if (running) {
-    setTimeout('animate()', 100);
-  }
 };
 
-function draw() {
-  for (var y = 0; y < rows; y++) {
-    for (var x = 0; x < cols; x++) {
-      var newColor = isSet(x, y, 'newColor');
-      if (newColor)
-        set(x, y, 'color', newColor);
-      var color = isSet(x, y, 'color');
-      canvas.fillStyle = color; 
-      canvas.fillRect(size * x, size * y, size, size);
-    }
+function animate() {
+  game.runRules();
+  canvas.draw();
+  if (running) {
+    setTimeout('animate()', 100);
   }
 };
