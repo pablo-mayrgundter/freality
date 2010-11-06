b4af90e7154f74137807228a9f9d5fe3f8a5fa34
pablo.mayrgundter <pablo.mayrgundter@b8c78ab8-d18f-11de-aacf-132551bdafba>
2010-11-06 20:51:11 +0000

    Misc edits.

--- ai/spatial/SineEnvironment.java
+++ ai/spatial/SineEnvironment.java
@@ -2,7 +2,9 @@ package ai.spatial;
 
 import ai.Environment;
 
-class SineEnvironment extends Environment<Double> {
+public class SineEnvironment extends Environment<Double> {
+  public SineEnvironment() {
+  }
   @Override
   public void set(final Double output) {
     state = 2.0 * Math.sin(state);
