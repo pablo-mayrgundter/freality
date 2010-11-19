d15917b8bc312fac99cc7a06350edfc7257db1a6
pablo.mayrgundter <pablo.mayrgundter@b8c78ab8-d18f-11de-aacf-132551bdafba>
2010-11-19 05:51:40 +0000

    Working with variable size, full-screen, click editing and fade; back to non-buggy GOL as default game.

--- fun/game/Canvas.js
+++ fun/game/Canvas.js
@@ -1,61 +1,72 @@
-var white = 'rgb(255,255,255)';
-var light = 'rgb(255,192,192)';
-var medium = 'rgb(192,192,64)';
-var dark = 'rgb(64,64,192)';
-var black = 'rgb(0,0,255)';
-var green = 'rgb(0,255,0)';
-
-function Canvas(board) {
+function Canvas(elt, board, cellSize) {
+  this.canvasElt = elt;
   this.board = board;
-  this.canvasElt = document.getElementById('game');
+  this.cellSize = cellSize;
   this.canvas = this.canvasElt.getContext('2d');
-  this.cW = this.canvasElt.width;
-  this.cH = this.canvasElt.height;
-  this.rows = Math.floor(this.cH / this.size);
-  this.cols = Math.floor(this.cW / this.size);
-  this.size = board.getSize();
-  for (var y = 0; y < this.size; y++) {
-    for (var x = 0; x < this.size; x++) {
-      this.board.set(x, y, 'age', 0);
+  for (var y = 0; y < board.getHeight(); y++) {
+    for (var x = 0; x < board.getWidth(); x++) {
+      try {
+        this.board.set(x, y, 'age', 0);
+      } catch(e) {
+        alert(x+','+y);
+        return;
+      }
     }
   }
 };
 
-Canvas.prototype.draw = function(expire) {
-  for (var y = 0; y < this.size; y++) {
-    for (var x = 0; x < this.size; x++) {
-      var fillColor = white, age;
+Canvas.prototype.draw = function() {
+  for (var y = 0; y < this.board.getHeight(); y++) {
+    for (var x = 0; x < this.board.getWidth(); x++) {
+      var fillColor = white;
       if (this.board.isOn(x, y)) {
-        fillColor = black;
         this.board.set(x, y, 'age', 4);
-      } else if (expire) {
-        var age = this.board.isSet(x, y, 'age');
-        this.board.set(x, y, 'age', --age)
-        switch(age) {
-        case 3: fillColor = dark; break;
-        case 2: fillColor = medium; break;
-        case 1: fillColor = light; break;
-        default:
-          fillColor = white;
-          this.board.set(x, y, 'age', 0);
-        }
+        fillColor = black;
+      } else {
+        this.board.set(x, y, 'age', this.board.isSet(x, y, 'age') - 1);
       }
-      this.canvas.fillStyle = fillColor;
-      this.canvas.fillRect(this.size * x, this.size * y, this.size, this.size);
+
+      switch(this.board.isSet(x, y, 'age')) {
+      case 4: fillColor = black; break;
+      case 3: fillColor = dark; break;
+      case 2: fillColor = medium; break;
+      case 1: fillColor = light;
+      }
+      this.drawCell(x, y, fillColor);
     }
   }
+};
+
+Canvas.prototype.drawCell = function(x, y, color) {
+  this.canvas.fillStyle = color;
+  this.canvas.fillRect(x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize);
 };
 
 Canvas.prototype.clickHandler = function() {
+  if (window.event.altKey) {
+    if (running) {
+      running = false;
+    } else {
+      running = true;
+      animate();
+    }
+    return;
+  }
+  var me = this;
   var evt = window.event;
-  var x = evt.x - this.canvasElt.offsetLeft;
-  var y = evt.y - this.canvasElt.offsetTop;
-  var dx = Math.floor(x / this.cW * this.size);
-  var dy = Math.floor(y / this.cH * this.size);
-  if (this.board.isOn(dx, dy))
-    this.board.turnOff(dx, dy);
-  else
-    this.board.turnOn(dx, dy);
-  this.board.next();
-  this.draw(false);
+  var cX = evt.x - me.canvasElt.offsetLeft;
+  var cY = evt.y - me.canvasElt.offsetTop;
+  var cW = me.canvasElt.width;
+  var cH = me.canvasElt.height;
+  var bX = Math.floor(cX / cW * board.getWidth());
+  var bY = Math.floor(cY / cH * board.getHeight());
+  if (me.board.isOn(bX, bY)) {
+    me.board.turnCurOff(bX, bY);
+    me.board.turnOff(bX, bY);
+    me.drawCell(bX, bY, white);
+  } else {
+    me.board.turnCurOn(bX, bY);
+    me.board.turnOn(bX, bY);
+    me.drawCell(bX, bY, black);
+  }
 };
--- fun/game/Game.js
+++ fun/game/Game.js
@@ -1,32 +1,39 @@
 var canvas, game, board;
+var white = 'rgb(255,255,255)';
+var light = 'rgb(192,192,255)';
+var medium = 'rgb(255,192,128)';
+var dark = 'rgb(0,64,128)';
+var black = 'rgb(0,0,0)';
+var cellSize = 20, inset = 0;
 
 function init() {
-  var boardSize = 25;
-  board = new Board(boardSize);
-  canvas = new Canvas(board);
+  var canvasElt = document.getElementById('game');
+  //canvasElt.style.width = (window.innerWidth - inset) +'px';
+  //canvasElt.style.height = (window.innerHeight - inset) +'px';
+  canvasElt.style.width = '1280px';
+  canvasElt.style.height = '800px'
+  var width = canvasElt.clientWidth / cellSize;
+  var height = canvasElt.clientHeight / cellSize;
+  board = new Board(width, height);
+  canvas = new Canvas(canvasElt, board, cellSize);
   game = new GOL(board);
-  addEvent(canvas.canvasElt, 'click', func(canvas, canvas.clickHandler));
-  addEvent(get('button'), 'click', buttonHandler);
+  addEvent(canvasElt, 'click', func(canvas, canvas.clickHandler));
+  board.turnOn(3, 1);
+  board.turnOn(3, 2);
+  board.turnOn(3, 3);
+  board.turnOn(2, 3);
+  board.turnOn(1, 2);
+  board.next();
+  canvas.draw();
 };
 
 var running = false;
 
-function buttonHandler(evt) {
-  if (evt) {
-    if (running)
-      evt.target.innerHTML = 'start';
-    else
-      evt.target.innerHTML = 'stop';
-  }
-  running = !running;
-  animate();
-};
-
 function animate() {
-  game.runRules();
-  canvas.draw(true);
-  board.next();
   if (running) {
-    setTimeout('animate()', 500);
+    game.runRules();
+    canvas.draw();
+    board.next();
+    setTimeout('animate()', 20);
   }
 };
