a2f34a82fa0d970f0a9c79750e9f7d0c06130c24
pablo.mayrgundter <pablo.mayrgundter@b8c78ab8-d18f-11de-aacf-132551bdafba>
2011-04-04 14:49:32 +0000

    Factored out some drawing.

--- fun/grid/Grid.java
+++ fun/grid/Grid.java
@@ -1,73 +1,45 @@
-package grid;
+package fun.grid;
 
+import gfx.FullScreenableFrame;
+import gfx.LayeredImage;
 import java.awt.*;
 import java.awt.image.*;
 import javax.swing.*;
 
 class Grid {
 
-  static int fanout = 4;
-  static int count = 0;
   final Grid [] neigh;
   final int level;
-  Object val;
+  final LayeredImage layers;
 
-  Grid (final int level) {
+  Grid(final int level, final LayeredImage layers) {
     this.level = level;
+    this.layers = layers;
     if (level == 1) {
       neigh = null;
     } else {
-      neigh = new Grid[fanout];
+      neigh = new Grid[FANOUT];
       for (int i = 0; i < 4; i++)
-        neigh[i] = new Grid(level - 1);
+        neigh[i] = new Grid(level - 1, layers);
     }
-    count++;
-  }
-
-  static final int XOFF = Integer.parseInt(System.getProperty("xoff", "0"));
-  static final int YOFF = Integer.parseInt(System.getProperty("yoff", "0"));
-  static final int WIDTH = Integer.parseInt(System.getProperty("width", "800"));
-  static final int HEIGHT = Integer.parseInt(System.getProperty("height", "800"));
-  static final int SLEEP = Integer.parseInt(System.getProperty("sleep", "30"));
-  static final boolean FULLSCREEN = Boolean.parseBoolean(System.getProperty("fs", "false"));
-  static final int levels = Integer.parseInt(System.getProperty("levels", "7"));
-  static final int size = Integer.parseInt(System.getProperty("size", "5"));
-  static final int w = size, h = size;
-  static final int [] path = new int[levels];
-  static void genPath() {
-    for (int i = 0; i < levels; i++) {
-      path[i] = (int)(Math.random() * 4.0);
-    }
-    path[levels-1] = 0;
-  }
-  static final BufferedImage [] lvlImgs = new BufferedImage[levels];
-  static {
-    for (int i = 0; i < levels; i++) {
-      lvlImgs[i] = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
-    }
-  }
-
-  static void setColor(final Graphics g, final float level, final int pathType) {
-    final float l = level/levels;
-    g.setColor(new Color(0.75f*l + (float)pathType / 32f, 0.5f*l + (float)pathType / 16f, (float)pathType/8f));
   }
 
   void draw(final int x, final int y, final int pos, int pathType) {
-    // Pretraversal draw.
-    Graphics g = lvlImgs[level - 1].getGraphics();
+    // Fill draw.
+    Graphics g = layers.getGraphics(level - 1);
     setColor(g, levels - level, 0);
     int off = w * (int)Math.pow(2, level - 1);
     if (pathType > 0 && level < levels) {
-      if (path[level - 1] == pos) {
-        setColor(g, level, pathType);
-        g.fillRect(x - w/2 + off/2, y - h/2 + off/2, off, off);
-      } else {
+      if (path[level - 1] != pos)
         return;
-      }
+      setColor(g, level, pathType);
+      g.fillRect(x - w/2 + off/2, y - h/2 + off/2, off, off);
     }
-    // grid oval.
-    lvlImgs[0].getGraphics().drawRect(x - w/2 + off/2, y - h/2 + off/2, off, off);
 
+    // Grid draw.
+    layers.getGraphics(0).drawRect(x - w/2 + off/2, y - h/2 + off/2, off, off);
+
+    // Recurse.
     if (level > 1) {
       off = w * (int)Math.pow(2, level - 1);
       neigh[0].draw(x, y, 0, pathType);
@@ -77,32 +49,46 @@ class Grid {
     }
   }
 
-  public static void main(final String [] args) {
-    final Frame f = new Frame();
-    f.setUndecorated(true);
-    f.setVisible(true);
-    f.setSize(WIDTH, HEIGHT);
-    if (FULLSCREEN) {
-      final GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
-      final GraphicsDevice device = env.getDefaultScreenDevice();
-      final GraphicsConfiguration config = device.getDefaultConfiguration();
-      device.setFullScreenWindow(f);
-    }
-    final Grid grid = new Grid(levels);
-    System.out.println("leafs: "+ count);
-    final Graphics g2d = f.getGraphics();
-    //((Graphics2D)g2d).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-    final BufferedImage compositeImg = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+  void setColor(final Graphics g, final float level, final int pathType) {
+    final float l = level/levels;
+    // Arbitrary color channel separation.
+    g.setColor(new Color(0.75f*l + (float)pathType / 32f,
+                         0.5f*l + (float)pathType / 16f,
+                         (float)pathType/8f));
+  }
 
+  static int prop(final String name, final int defaultVal) {
+    return Integer.parseInt(System.getProperty(name, ""+defaultVal));
+  }
+
+  static final int FANOUT = 4;
+  static final int levels = prop("levels", 7);
+  static final int size = prop("size", 5);
+  static final int w = size, h = size;
+  static final int SLEEP = prop("sleep", 100);
+  static final int [] path = new int[levels];
+  static void genPath() {
+    for (int i = 0; i < levels; i++) {
+      path[i] = (int)(Math.random() * 4.0);
+    }
+    path[levels-1] = 0;
+  }
+
+  public static void main(final String [] args) {
+    final FullScreenableFrame f = new FullScreenableFrame();
+    final LayeredImage layers = new LayeredImage(levels, f.getWidth(), f.getHeight());
+    final Grid grid = new Grid(levels, layers);
+    final Graphics g2d = f.getDrawGraphics();
+    final BufferedImage compositeImg =
+      new BufferedImage(f.getWidth(), f.getHeight(), BufferedImage.TYPE_INT_ARGB);
     // Grid is expensive to draw, render only once.
-    //grid.draw(XOFF, YOFF, 0, 0);
+    if (Boolean.getBoolean("grid"))
+      grid.draw(0, 0, 0, 0);
 
     while (true) {
       genPath();
-      grid.draw(XOFF, YOFF, 0, 1 + (int)(Math.random() * 7.0));
-      for (int i = levels - 1; i >= 0; i--) {
-        compositeImg.getGraphics().drawImage(lvlImgs[i], 0, 0, null);
-      }
+      grid.draw(0, 0, 0, 1 + (int)(Math.random() * 7.0));
+      layers.draw(compositeImg.getGraphics());
       g2d.drawImage(compositeImg, 0, 0, null);
       try { Thread.sleep(SLEEP); } catch(Exception e) { break; }
     }
