04e767cc577f0568d9f8a311e2b6aa73eecede0e
pablo.mayrgundter@gmail.com <pablo.mayrgundter@gmail.com@b8c78ab8-d18f-11de-aacf-132551bdafba>
2011-08-15 17:54:52 +0000

    Refactoring for multiple textures; added mime-type properties to servable content.

--- trunk/fun/webgl/texture.js
+++ trunk/fun/webgl/texture.js
@@ -1,20 +1,24 @@
-var theTexture;
+function Texture(src) {
+  this.theTexture = null;
+  this.texSource = src;
 
-function handleLoadedTexture(texture) {
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-  gl.bindTexture(gl.TEXTURE_2D, null);
-}
-
-function initTexture() {
-  theTexture = gl.createTexture();
-  theTexture.image = new Image();
-  theTexture.image.onload = function () {
-    handleLoadedTexture(theTexture)
-  }
+  Texture.prototype.init = function() {
+    this.theTexture = gl.createTexture();
+    this.theTexture.image = new Image();
+    var me = this;
+    this.theTexture.image.onload = function () {
+      me.handleLoad(me.theTexture)
+    }
  
-  theTexture.image.src = 'earth.jpg';
+    this.theTexture.image.src = this.texSource;
+  }
+
+  Texture.prototype.handleLoad = function(texture) {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+  }
 }
