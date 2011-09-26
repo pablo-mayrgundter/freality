7f6cb4f2c656da8403d5f647622c8971f475b731
pablo.mayrgundter@gmail.com <pablo.mayrgundter@gmail.com@b8c78ab8-d18f-11de-aacf-132551bdafba>
2011-09-26 01:16:57 +0000

    Running again, added multi-processor.

--- trunk/math/util/Linear.java
+++ trunk/math/util/Linear.java
@@ -1,17 +1,25 @@
 package math.util;
 
+/**
+ * Linear math helper functions.
+ *
+ * @author Pablo Mayrgundter
+ */
 public final class Linear {
+
   /**
-   * Squeeze b between and c.
-   * @return max(a, min(b, c)
+   * Squeeze b between a and c and truncate the result to an int.
+   *
+   * @return max(a, min(b, c))
    */
   public static int squeezeToInt(final double a, final double b, final double c) {
     return (int) Math.max(a, Math.min(b, c));
   }
 
   /**
-   * Squeeze b between and c.
-   * @return max(a, min(b, c)
+   * Squeeze b between a and c.
+   *
+   * @return max(a, min(b, c))
    */
   public static double squeeze(final double a, final double b, final double c) {
     return Math.max(a, Math.min(b, c));
