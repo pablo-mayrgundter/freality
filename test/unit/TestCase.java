a9bbae65a556741ca735fa5cbc12983b872c7e60
pablo.mayrgundter <pablo.mayrgundter@gmail.com>
2013-12-01 05:01:21 +0000

    Use common base-class for unit tests and precondition checks.

--- test/unit/TestCase.java
+++ test/unit/TestCase.java
@@ -1,13 +1,20 @@
 package unit;
 
+import util.Check;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TestCase {
+public class TestCase extends Check<TestCase.TestException> {
+
+  static class TestException extends RuntimeException {
+    TestException(String msg) {
+      super(msg);
+    }
+  }
 
   enum Result {
     PASS("."), FAIL("F"), ERR("E");
@@ -21,18 +28,17 @@ public class TestCase {
     }
   }
 
-  static class TestException extends RuntimeException {
-    TestException(String msg) {
-      super(msg);
-    }
-  }
-
   final List<String> errorMsgs;
   final List<Result> results;
 
   public TestCase() {
     errorMsgs = new ArrayList<String>();
     results = new ArrayList<Result>();
+  }
+
+  @Override
+  protected void throwException(String msg) throws TestException {
+    throw new TestException(msg);
   }
 
   @Override
@@ -49,83 +55,6 @@ public class TestCase {
 
   protected void setUp() {}
   protected void tearDown() {}
-
-  protected void assertEquals(double a, double b) {
-    assertEquals(new Double(a), new Double(b),
-                 String.format("Expected %f, got %f", a, b));
-  }
-
-  protected void assertEquals(double[] a, double[] b, Object ... msg) {
-    if (Arrays.equals(a, b)) {
-      return;
-    }
-    throwMsgOrDiff(a, b, msg);
-  }
-
-  /** Prefer the msg if present over diff. */
-  void throwMsgOrDiff(double[] a, double[] b, Object ... msg) {
-    throw new TestException(msg.length > 0 ? format(msg) : diff(a, b));
-  }
-
-  String diff(double[] a, double[] b) {
-    String msg = null;
-    if (a.length != b.length) {
-      msg = "Array lengths differ";
-    } else {
-      for (int i = 0; i < a.length; i++) {
-        double aElt = a[i];
-        double bElt = b[i];
-        if (aElt != bElt) {
-          msg = String.format("elts at index %d differ: %s != %s", i, aElt, bElt);
-          break;
-        }
-      }
-    }
-    if (msg == null) throw new IllegalStateException("Couldn't find diff");
-    return msg;
-  }
-
-  protected void assertEquals(Object a, Object b, Object ... msg) {
-    if (a == null && b == null) {
-      return;
-    }
-    if (!a.equals(b)) {
-      throw new TestException(format(msg));
-    }
-  }
-
-  protected void assertTrue(boolean b, Object ... msg) {
-    if (!b) {
-      throw new TestException(format(msg));
-    }
-  }
-
-  protected void assertFalse(boolean b, Object ... msg) {
-    if (b) {
-      throw new TestException(format(msg));
-    }
-  }
-
-  protected void fail(Object ... msg) {
-    throw new TestException(format(msg));
-  }
-
-  String format(Object ... msg) {
-    if (msg.length > 0) {
-      if (!(msg[0] instanceof String)) {
-        throw new IllegalArgumentException("First element of varargs must be a string");
-      }
-      String fmt = (String) msg[0];
-      if (msg.length == 1) {
-        return fmt;
-      } else if (msg.length > 1) {
-        Object [] args = new Object[msg.length - 1];
-        System.arraycopy(msg, 1, args, 0, msg.length - 1);
-        return String.format(fmt, args);
-      }
-    }
-    return "";
-  }
 
   public TestCase run() {
     setUp();
