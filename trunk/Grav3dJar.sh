8657fc462577a2f7d7f0238639633f8fe7f2835e
pablo.mayrgundter@gmail.com <pablo.mayrgundter@gmail.com@b8c78ab8-d18f-11de-aacf-132551bdafba>
2014-03-10 02:56:11 +0000

    consolidating directories

--- trunk/Grav3dJar.sh
+++
@@ -1,3 +0,0 @@
-JAR=Grav3d.jar
-jar cmf Grav3d.mf $JAR phys/{Grav*,Phys*,GravSim*,Point*,Blob*,ColoredBlob*,Display*}.{java,class} gfx/Display3D*.{java,class} gfx/FullScreenableFrame.{java,class} org/freality/gui vr/cpack/space/SpaceShipNavigator.*
-jarsigner -keystore myKeys $JAR freality.googlecode.com
--- trunk/SolarSystem3d.sh
+++
@@ -1,7 +0,0 @@
-LOG=SolarSystem3d.log
-JAR=SolarSystem3d.jar
-CLASSES=`cat SolarSystem3d.log | egrep -v 'Loaded (com.|java.|javax.|org.xml.|sun.|apple.)' | sed 's/\./\//g' | awk '/Loaded/{print $2".class"}' | sort | uniq`
-SRCS=`cat SolarSystem3d.log | grep Loaded | egrep -v 'Loaded (com.|java.|javax.|org.xml.|sun.|apple.)' | sed -e 's/$.*//g' -e 's/\./\//g' | awk '/Loaded/{print $2".java"}' | sort | uniq`
-DATA="vr/cpack/space/data/solarSystem.xml vr/cpack/space/textures/*.jpg vr/cpack/space/data/hygfull.csv.gz"
-jar cmf SolarSystem3d.mf $JAR $CLASSES $SCRS $DATA
-jarsigner -keystore myKeys $JAR freality.googlecode.com
