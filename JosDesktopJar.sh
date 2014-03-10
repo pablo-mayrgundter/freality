d9f34e5ebafa50227ce262db07febed89ac8291d
pablo.mayrgundter <pablo.mayrgundter@gmail.com>
2014-03-10 02:56:11 +0000

    consolidating directories

--- JosDesktopJar.sh
+++
@@ -1,8 +0,0 @@
-NAME=JosDesktop
-LOG=$NAME.log
-JAR=$NAME.jar
-CLASSES=`cat $LOG | egrep -v 'Loaded (com.|java.|javax.|org.xml.|sun.|apple.)' | sed 's/\./\//g' | awk '/Loaded/{print $2".class"}' | sort | uniq`
-SRCS=`cat $LOG | grep Loaded | egrep -v 'Loaded (com.|java.|javax.|org.xml.|sun.|apple.)' | sed -e 's/$.*//g' -e 's/\./\//g' | awk '/Loaded/{print $2".java"}' | sort | uniq`
-DATA="jos/desktop/*.properties desktop.jpg"
-jar cmf $NAME.mf $JAR $CLASSES $SCRS $DATA
-jarsigner -keystore myKeys $JAR freality.googlecode.com
