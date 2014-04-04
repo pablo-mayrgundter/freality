a4a2fbdf7280ad025771ce77bd26d90d26c8655e
pablo.mayrgundter <pablo.mayrgundter@gmail.com>
2014-04-04 22:33:35 +0000

    Palette fix.  User input for wall position.  Bigger cells.

--- phys/fluid/Color.dart
+++ phys/fluid/Color.dart
@@ -4,9 +4,15 @@ class Color {
   int r, g, b;
   Color(this.r, this.g, this.b);
   String toCssString() {
-    var rs = r == 0 ? '00' : r.toRadixString(16);
-    var gs = g == 0 ? '00' : g.toRadixString(16);
-    var bs = b == 0 ? '00' : b.toRadixString(16);
+    var rs = r.toRadixString(16);
+    if (r < 16)
+      rs = '0' + rs;
+    var gs = g.toRadixString(16);
+    if (g < 16)
+      gs = '0' + gs;
+    var bs = b.toRadixString(16);
+    if (b < 16)
+      bs = '0' + bs;
     return '#' + rs + gs + bs;
   }
 }
