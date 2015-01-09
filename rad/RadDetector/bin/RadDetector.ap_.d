6428749e9d9e625f5e19f706f4c0fb47d3e05df6
pablo.mayrgundter <pablo.mayrgundter@gmail.com>
2015-01-09 22:16:30 +0000

    RadDetector

---
+++ rad/RadDetector/bin/RadDetector.ap_.d
@@ -0,0 +1,12 @@
+/Users/pmy/freality/rad/RadDetector/bin/RadDetector.ap_ \
+ : /Users/pmy/freality/rad/RadDetector/res/drawable-hdpi/ic_launcher.png \
+/Users/pmy/freality/rad/RadDetector/res/drawable-ldpi/ic_launcher.png \
+/Users/pmy/freality/rad/RadDetector/res/drawable-mdpi/ic_launcher.png \
+/Users/pmy/freality/rad/RadDetector/res/drawable-xhdpi/ic_launcher.png \
+/Users/pmy/freality/rad/RadDetector/res/layout/main.xml \
+/Users/pmy/freality/rad/RadDetector/res/values/strings.xml \
+/Users/pmy/freality/rad/RadDetector/bin/res/drawable-hdpi/ic_launcher.png \
+/Users/pmy/freality/rad/RadDetector/bin/res/drawable-ldpi/ic_launcher.png \
+/Users/pmy/freality/rad/RadDetector/bin/res/drawable-mdpi/ic_launcher.png \
+/Users/pmy/freality/rad/RadDetector/bin/res/drawable-xhdpi/ic_launcher.png \
+/Users/pmy/freality/rad/RadDetector/bin/AndroidManifest.xml \
