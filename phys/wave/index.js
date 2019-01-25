160823fcbbddd2ac096a3298cdde0a4f7c250e1c
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2019-01-25 00:38:20 -0600

    phys/wave: pixel drawing; opti.

--- phys/wave/index.js
+++ phys/wave/index.js
@@ -6,7 +6,7 @@ function init() {
   canvas.width = size;
   canvas.height = size;
   c = canvas.getContext('2d');
-  c.scale(canvas.width, canvas.height);
+  c.scale(canvas.width / 2, canvas.height / 2);
   window.requestAnimationFrame(anim);
 }
 
@@ -15,16 +15,18 @@ let i = 0;
 function anim(timestamp) {
   const d = Math.PI * 2 * (i++ / 100);
   clear(c);
-  axes(c);
+  //axes(c);
   //sinX(c, d);
   //sinY(c, d);
-  pond(c, d);
+  //pixelWaves(c, d);
+  //mergeSplit(c, d);
+  rotateBlobs(c, d);
   window.requestAnimationFrame(anim);
 }
 
 function clear(c) {
   c.fillStyle = '#fff';
-  c.fillRect(0, 0, xToC(1), yToC(1));
+  c.fillRect(0, 0, xToC(4), yToC(4));
 }
 
 function line(c, x1, y1, x2, y2) {
@@ -41,18 +43,47 @@ function axes(c) {
   line(c,   0,  -1,   0,   1);
 }
 
-function pond(c, phase) {
+function rotateBlobs(c, phase) {
   const N = 100, Tau = 2 * Math.PI;
-  //c.fillStyle = `rgb(0, 0, ${intensity})`;
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      const x1 = -Math.PI + i / N * Tau;
+      const x2 = -Math.PI + j / N * Tau;
+      const y1 = Math.sin(x1 + phase) + Math.cos(x1 - phase);
+      const y2 = Math.sin(x2 - phase) + Math.cos(x2 + phase);
+      const intensity = 128 * y1 + 128 * y2;
+      c.fillStyle = `rgb(${intensity}, ${intensity}, ${intensity})`;
+      c.fillRect(xToC(x1), yToC(x2), 0.03, 0.03);
+    }
+  }
+}
+
+function mergeSplit(c, phase) {
+  const N = 100, Tau = 2 * Math.PI;
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      const x1 = -Math.PI + i / N * Tau;
+      const x2 = -Math.PI + j / N * Tau;
+      const y1 = Math.sin(x1 + phase);
+      const y2 = Math.cos(x2 + phase);
+      const y3 = Math.sin(-x1 + phase);
+      const y4 = Math.cos(-x2 + phase);
+      const intensity = 64 * y1 + 64 * y2 + 64 * y3 + 64 * y4;
+      c.fillStyle = `rgb(${intensity}, ${intensity}, ${intensity})`;
+      c.fillRect(xToC(x1), yToC(x2), 0.03, 0.03);
+    }
+  }
+}
+
+function pixelWaves(c, phase) {
+  const N = 100, Tau = 2 * Math.PI;
   c.fillStyle = '#000000';
   for (let i = 0; i < N; i++) {
-    //for (let j = 0; j < N; j++) {
-      const dX = i / N * Tau;
-      const x = -Math.PI + dX;
-      const y = Math.sin(dX + phase);
-      c.fillRect(xToC(x), yToC(y), 0.03, 0.03);
-      c.fillRect(xToC(y), yToC(x), 0.03, 0.03);
-    //}
+    const dX = i / N * Tau;
+    const x = -Math.PI + dX;
+    const y = Math.sin(dX + phase);
+    c.fillRect(xToC(x), yToC(y), 0.03, 0.03);
+    c.fillRect(xToC(y), yToC(x), 0.03, 0.03);
   }
 }
 
@@ -79,11 +110,11 @@ function sinY(c, phase) {
 }
 
 function xToC(x) {
-  return 0.5 * x + 0.5;
+  return 0.25 * x + 1;
 }
 
 function yToC(y) {
-  return 0.5 * y + 0.5;
+  return 0.25 * y + 1;
 }
 
 function begin(c) {
