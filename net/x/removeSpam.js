7faf8d7fc1caad322c5f7c6ed7920baf675dbc97
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2024-07-27 18:51:09 +0200

    added net/x/removeSpam.js

---
+++ net/x/removeSpam.js
@@ -0,0 +1,66 @@
+/**
+ * Usage:
+ *
+ * Paste this into console on your x.com/notifications page
+ *
+ * Then call removeFollow() once to remove the top follow notification
+ */
+
+function getElementByTextContent(selector, text, index = 0) {
+  const elements = document.querySelectorAll(selector)
+  const matches = []
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].textContent.trim().includes(text)) {
+      if (index === 0) {
+        return elements[i]
+      }
+      index--
+    }
+  }
+}
+
+function getFirstElementByTextContent(selector, text) {
+  return getElementByTextContent(selector, text)
+}
+
+function getFollow() {
+  const follow = getFirstElementByTextContent('span', 'followed you')
+  return follow.parentNode
+}
+
+const log = (msg) => console.log(msg)
+
+const pauseTime = 1000
+function removeFollow() {
+  log('removeFollow')
+  getFollow().click()
+  setTimeout(clickMore, pauseTime)
+}
+
+function clickMore() {
+  log('clickMore')
+  document.querySelector('[aria-label="More"]').click()
+  setTimeout(clickReport, pauseTime)
+}
+
+function clickReport() {
+  log('clickReport')
+  getFirstElementByTextContent('span', 'Report').click()
+  setTimeout(clickSpamNext, pauseTime)
+}
+
+function clickSpamNext() {
+  log('clickSpamNext')
+  getFirstElementByTextContent('span', 'Spam').click()
+  getFirstElementByTextContent('span', 'Next').click()
+  setTimeout(blockDoneBack, pauseTime)
+}
+
+function blockDoneBack() {
+  log('blockDoneBack')
+  getElementByTextContent('span', 'Block', 2).click()
+  getFirstElementByTextContent('span', 'Done').click()
+  document.querySelector('[aria-label="Back"]').click()
+}
+
+
